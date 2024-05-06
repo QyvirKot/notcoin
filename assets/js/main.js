f8570ae8a623e02c9e9292d893ae90a2da1fbda6
QyvirKot <168199043+QyvirKot@users.noreply.github.com>
2024-05-07 07:12:30 +1000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,45 @@
 const body = document.body;
 const image = body.querySelector('#coin');
 const h1 = body.querySelector('h1');
-localStorage.setItem('total', '1000');
-body.querySelector('#total').textContent = '/1000';
-localStorage.setItem('power', '100');
-body.querySelector('#power').textContent = '100';
-let coins = localStorage.getItem('coins');
-let total = localStorage.getItem('total');
-let power = localStorage.getItem('power');
-let count = localStorage.getItem('count')
+const totalElement = body.querySelector('#total');
+const powerElement = body.querySelector('#power');
+const progressElement = body.querySelector('.progress');
 
-if (coins == null) {
+localStorage.setItem('total', '1000');
+localStorage.setItem('power', '100');
+if (!localStorage.getItem('coins')) {
     localStorage.setItem('coins', '0');
     h1.textContent = '0';
 } else {
-    h1.textContent = Number(coins).toLocaleString();
+    h1.textContent = Number(localStorage.getItem('coins')).toLocaleString();
 }
-
-if (total == null) {
-    localStorage.setItem('total', '1000')
-    body.querySelector('#total').textContent = '/1000';
-} else {
-    body.querySelector('#total').textContent = `/${total}`;
-}
-
-if (power == null) {
-    localStorage.setItem('power', '100');
-    body.querySelector('#power').textContent = '100';
-} else {
-    body.querySelector('#power').textContent = power;
-}
-
-if (count == null) {
-    localStorage.setItem('count', '1')
-}
+totalElement.textContent = '/1000';
+powerElement.textContent = '100';
 
 image.addEventListener('touchstart', (e) => {
-
-    let x = e.touches[0].clientX;
-    let y = e.touches[0].clientY;
-
+    e.preventDefault();
+    const x = e.touches[0].clientX;
+    const y = e.touches[0].clientY;
     navigator.vibrate(5);
 
-    coins = localStorage.getItem('coins');
-    power = localStorage.getItem('power');
+    let coins = parseInt(localStorage.getItem('coins'));
+    let power = parseInt(localStorage.getItem('power'));
 
-    if (Number(power) > 0) {
-        localStorage.setItem('coins', `${Number(coins) + 1}`);
-        h1.textContent = `${(Number(coins) + 1).toLocaleString()}`;
+    if (power > 0) {
+        localStorage.setItem('coins', coins + 1);
+        h1.textContent = (coins + 1).toLocaleString();
 
-        localStorage.setItem('power', `${Number(power) - 1}`);
-        body.querySelector('#power').textContent = `${Number(power) - 1}`;
+        localStorage.setItem('power', power - 1);
+        powerElement.textContent = power - 1;
     }
 
-    if (x < 150 & y < 150) {
+    if (x < 150 && y < 150) {
         image.style.transform = 'translate(-0.25rem, -0.25rem) skewY(-10deg) skewX(5deg)';
-    } else if (x < 150 & y > 150) {
+    } else if (x < 150 && y > 150) {
         image.style.transform = 'translate(-0.25rem, 0.25rem) skewY(-10deg) skewX(5deg)';
-    } else if (x > 150 & y > 150) {
+    } else if (x > 150 && y > 150) {
         image.style.transform = 'translate(0.25rem, 0.25rem) skewY(10deg) skewX(-5deg)';
-    } else if (x > 150 & y < 150) {
+    } else if (x > 150 && y < 150) {
         image.style.transform = 'translate(0.25rem, -0.25rem) skewY(10deg) skewX(-5deg)';
     }
 
@@ -67,15 +47,14 @@ image.addEventListener('touchstart', (e) => {
         image.style.transform = 'translate(0px, 0px)';
     }, 100);
 
-    body.querySelector('.progress').style.width = `${(100 * power) / total}%`;
+    progressElement.style.width = `${(100 * power) / 1000}%`;
 });
 
 setInterval(() => {
-    count = localStorage.getItem('count')
-    power = localStorage.getItem('power');
-    if (Number(total) > power) {
-        localStorage.setItem('power', `${Number(power) + Number(count)}`);
-        body.querySelector('#power').textContent = `${Number(power) + Number(count)}`;
-        body.querySelector('.progress').style.width = `${(100 * power) / total}%`;
+    let power = parseInt(localStorage.getItem('power'));
+    if (power < 100) {
+        localStorage.setItem('power', power + 1);
+        powerElement.textContent = power + 1;
+        progressElement.style.width = `${(100 * (power + 1)) / 1000}%`;
     }
 }, 100);

fc359744fc913fb5f7f2e10cb856dc8f7ef889c1
QyvirKot <168199043+QyvirKot@users.noreply.github.com>
2025-02-14 21:01:12 +1000

    Update tonsdk.js

--- tonsdk.js
+++ tonsdk.js
@@ -53,7 +53,7 @@ async function updateBalance(walletAddress) {
         // Обновляем отображение баланса на странице
         const walletInfoElement = document.getElementById('wallet-info');
         if (walletInfoElement) {
-            walletInfoElement.innerHTML = `Remaining amount of activations: <b>${displayedBalance} TON</b>`;
+            walletInfoElement.innerHTML = `Remaining amount of reward: <b>${displayedBalance} TON</b>`;
         }
 
     } catch (error) {
